31a16b485084e9ebc10cf3a2d77992f64ed02096
cargnieli <cargnieli@yahoo.com>
2021-05-23 10:48:41 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int x;
+
+	for(x = 0; x<20; x = x+1)
+        System.out.println("O valor de x é: " + x);
+    }
+}
